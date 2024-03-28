d7add7c351741fa1995bd68cd2f636a7ff4856d9
Shoykat361 <shoykat15-11724@diu.edu.bd>
2024-03-29 02:03:04 +0600

    Richtext learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World"),
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        "Hello World",
+        style: TextStyle(
+            fontSize: 20.00, fontWeight: FontWeight.bold, color: Colors.red),
       ),
-      body: Center(
-        child: Text('Hello Shoikat')
-      ),
+      backgroundColor: Colors.blue,
+    ),
+    /* body: const Center(
+            child: Text(
+          'Hello Shoikat',
+          style: TextStyle(
+              fontSize: 20.00, fontWeight: FontWeight.bold, color: Colors.cyan),
+        ))),*/
+    body: RichText(
+      text: const TextSpan(
+          text: 'Hel',
+          style: TextStyle(
+              fontSize: 30.00, color: Colors.red, fontStyle: FontStyle.italic),
+        children: [
+          TextSpan(text: 'llo',style: TextStyle(fontSize: 40.00, color: Colors.cyan, fontWeight: FontWeight.bold)),
+          TextSpan(text: ' This',style: TextStyle(fontSize: 30.00, color: Colors.purple, fontStyle: FontStyle.italic)),
+          TextSpan(text: ' Is',style: TextStyle(fontSize: 40.00, color: Colors.amber, fontWeight: FontWeight.bold)),
+          TextSpan(text: ' saha',style: TextStyle(fontSize: 30.00, color: Colors.blue, fontStyle: FontStyle.italic)),
+        ],
 
+      ),
+    ),
   )));
 }
-
-
