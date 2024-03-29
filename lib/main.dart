148b45eec2e5759896a5d41b13995c185468472a
Shoykat361 <shoykat15-11724@diu.edu.bd>
2024-03-29 13:57:23 +0600

    Row and column and mainaxixsize

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,49 @@ void main() {
           style: TextStyle(
               fontSize: 20.00, fontWeight: FontWeight.bold, color: Colors.cyan),
         ))),*/
-    body: RichText(
-      text: const TextSpan(
-          text: 'Hel',
-          style: TextStyle(
-              fontSize: 30.00, color: Colors.red, fontStyle: FontStyle.italic),
-        children: [
-          TextSpan(text: 'llo',style: TextStyle(fontSize: 40.00, color: Colors.cyan, fontWeight: FontWeight.bold)),
-          TextSpan(text: ' This',style: TextStyle(fontSize: 30.00, color: Colors.purple, fontStyle: FontStyle.italic)),
-          TextSpan(text: ' Is',style: TextStyle(fontSize: 40.00, color: Colors.amber, fontWeight: FontWeight.bold)),
-          TextSpan(text: ' saha',style: TextStyle(fontSize: 30.00, color: Colors.blue, fontStyle: FontStyle.italic)),
-        ],
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
 
-      ),
-    ),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.ice_skating,size: 60,),
+                Icon(Icons.cable_rounded,size: 60,),
+                
+              ],
+            ),
+            RichText(
+              text: const TextSpan(
+                text: 'Hel',
+                style: TextStyle(
+                    fontSize: 30.00, color: Colors.red, fontStyle: FontStyle.italic),
+                children: [
+                  TextSpan(text: 'llo',style: TextStyle(fontSize: 40.00, color: Colors.cyan, fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' This',style: TextStyle(fontSize: 30.00, color: Colors.purple, fontStyle: FontStyle.italic)),
+                  TextSpan(text: ' Is',style: TextStyle(fontSize: 40.00, color: Colors.amber, fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' saha',style: TextStyle(fontSize: 30.00, color: Colors.blue, fontStyle: FontStyle.italic)),
+                ],
+
+              ),
+            ),
+            RichText(
+              text: const TextSpan(
+                text: 'Hel',
+                style: TextStyle(
+                    fontSize: 30.00, color: Colors.red, fontStyle: FontStyle.italic),
+                children: [
+                  TextSpan(text: 'llo',style: TextStyle(fontSize: 40.00, color: Colors.cyan, fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' This',style: TextStyle(fontSize: 30.00, color: Colors.purple, fontStyle: FontStyle.italic)),
+                  TextSpan(text: ' Is',style: TextStyle(fontSize: 40.00, color: Colors.amber, fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' saha',style: TextStyle(fontSize: 30.00, color: Colors.blue, fontStyle: FontStyle.italic)),
+                ],
+
+              ),
+            ),
+          ],
+        ),
+
   )));
 }
